e4fa1d27b0fc7f73d000299326657e9602466eb0
bjoukovs <borisjoukov@hotmail.com>
2017-04-05 20:31:53 +0200

    corrections

--- Rayon.py
+++ Rayon.py
@@ -6,10 +6,10 @@ class rayon(object):
     def __init__(self): 
         points.append(point(TXx, TXy))
 
-    def add_point(point)
+    def add_point(point):
         points.append(point)
 
-    def add_points(point)
+    def add_points(point):
         points.extend(point)
 
     def intersect (p1,p2,murs):
@@ -18,13 +18,13 @@ class rayon(object):
         for mur in murs :
             if (mur.x1==mur.x2): 
                 pty= direction * mur.x1
-                if (mur.get_ymin <= pty <= mur.get_ymax):
+                if (mur.get_ymin() <= pty <= mur.get_ymax()):
                     p = point (mur.x1,pty)
                     p.set_mur(mur)
                     ptintersects.append(p)
             if ( mur.y1==mur.y2):
                 ptx = mur.y1/ direction 
-                if (mur.get_xmin <= ptx <= mur.get_xmax):
+                if (mur.get_xmin() <= ptx <= mur.get_xmax()):
                     p = point (ptx,mur.y1)
                     p.set_mur(mur)
                     ptintersects.append(p)        
--- point.py
+++ point.py
@@ -5,14 +5,14 @@ class point(object):
     y = 0
     mur = None 
 
-    def __init__(self, x, y)
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def set_mur(self, mur)
+    def set_mur(self, mur):
         self.mur = mur
     
-    def find_mur(self, murs)
+    def find_mur(self, murs):
         c
         for mur in murs:
             if((mur.x1 == x & mur.x2 ==x && ((y<=mur.get_ymax() && y>=mur.get_ymin()))) || (mur.y1 == y & mur.y2 ==y && ((x<=mur.get_xmax() && x>=mur.get_xmin())))):
