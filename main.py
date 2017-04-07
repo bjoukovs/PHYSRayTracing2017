b2373c79b0555b3de9ed0b881218501d59a51b71
bjoukovs <borisjoukov@hotmail.com>
2017-04-07 17:39:58 +0200

    rays graphics

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ print(len(MURS))
 COINS = data[8]
 width, height, TXx, TXy, TXorientation, RXx, RXy = data[0],data[1],data[2],data[3],data[4],data[5],data[6]
 
-draw(MURS, width, height, TXx, TXy, RXx, RXy) 
+RAYS_REFLEXION = rayons_reflexion(Point(TXx, TXy), Point(RXx, RXy), MURS)
 
-rayons_reflexion(Point(TXx, TXy), Point(RXx, RXy), MURS)
+draw(MURS, RAYS_REFLEXION, width, height, TXx, TXy, RXx, RXy) 
