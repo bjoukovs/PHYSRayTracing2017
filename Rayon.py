2e8dc1de1ce83959b7da1932f589bdd5ccba03a8
Maxence Draguet <maxdraguet@hotmail.com>
2017-04-07 13:44:59 +0200

    réflexions début

--- Rayon.py
+++ Rayon.py
@@ -15,26 +15,45 @@ class Rayon(object):
         return self.points 
 
     def intersect (p1,p2,murs):
-        direction = (p2.y-p1.y)/(p2.x-p1.x)
-        ptintersects = []
-        for mur in murs :
-            if (mur.is_horizontal()): 
-                pty= direction * mur.coin1.x
-                if (mur.get_ymin() <= pty <= mur.get_ymax()):
-                    p = point (mur.coin1.x,pty)
-<<<<<<< HEAD
-=======
-                    p.set_mur(mur)
->>>>>>> 31c9a5c54e6dfb0f766db1d2f96f068f7b602dc5
-                    ptintersects.append(p)
-            else:
-                ptx = mur.coin1.y/ direction 
-                if (mur.get_xmin() <= ptx <= mur.get_xmax()):
-                    p = point (ptx,mur.coin1.y)
-<<<<<<< HEAD
-=======
-                    p.set_mur(mur)
->>>>>>> 31c9a5c54e6dfb0f766db1d2f96f068f7b602dc5
-                    ptintersects.append(p)        
+        if((p2.x-p1.x=!=0) and (p2.y-p1.y=!=0)):
+            direction = (p2.y-p1.y)/(p2.x-p1.x)
+            ptintersects = []
+            for mur in murs :
+                if (mur.is_horizontal()): 
+                    ptx= (mur.coin1.y-p1.y)/direction + p1.x
+                    if (mur.get_xmin() <= ptx <= mur.get_xmax() and mur.coin1.y=!= p1.y and mur.coin1.y=!= p2.y):
+                        p = point (ptx ,mur.coin1.y)
+    <<<<<<< HEAD
+    =======
+                        p.set_mur(mur)
+    >>>>>>> 31c9a5c54e6dfb0f766db1d2f96f068f7b602dc5
+                        ptintersects.append(p)
+                else:
+                    pty =(mur.coin1.x-p1.x)*direction + p1.y
+                    if (mur.get_xmin() <= ptx <= mur.get_xmax()and mur.coin1.x=!= p1.x and mur.coin1.y=!= p2.x):
+                        p = point (mur.coin1.x, pty)
+    <<<<<<< HEAD
+    =======
+                        p.set_mur(mur)
+    >>>>>>> 31c9a5c54e6dfb0f766db1d2f96f068f7b602dc5
+                        ptintersects.append(p) 
+        elif(p2.x== p1.x):
+            for mur in murs :
+                if (mur.is_horizontal()): 
+                    p = point (p1.x ,mur.coin1.y)
+    <<<<<<< HEAD
+    =======
+                        p.set_mur(mur)
+    >>>>>>> 31c9a5c54e6dfb0f766db1d2f96f068f7b602dc5
+                        ptintersects.append(p)
+        elif(p2.y==p1.y)
+            for mur in murs :
+                if (mur.is_horizontal()==False): 
+                    p = point (mur.coin1.x,p1.y)
+    <<<<<<< HEAD
+    =======
+                        p.set_mur(mur)
+    >>>>>>> 31c9a5c54e6dfb0f766db1d2f96f068f7b602dc5
+                        ptintersects.append(p)
         return ptintersects 
 
--- reflexion.py
+++ reflexion.py
@@ -16,13 +16,26 @@ def image_points(start_point, murs):
 def une_reflexion(start_point,end_point, murs):
      list_rayons = []
      image_elems = image_points(start_point, murs)
-     for elem in image_elems:
+     for elem in image_elems:                        #1reflexion
          murge =  [elem[2]]
          intersect_point = rayon.intersect (elem[1],end_point,murge) #attention  intersect_point est une liste et murge utilisé car la fonction nécessite une liste
-         nouveau_rayon = Rayon(start)
+         nouveau_rayon = Rayon(start_point)                        
+         nouveau_rayon.add_points(rayon.intersect (intersect_point,start_points,murs))  #intersection du rayon avec les murs avants réflexions
+         nouveau_rayon.add_point([intersect_point)
+         nouveau_rayon.add_points(rayon.intersect (intersect_point,end_points,murs))  #intersection du rayon avec les murs après réflexions
+         nouveau_rayon.add_point(end_point)                                           #point final du nouveau rayon
+         list_rayons.append(nouveau_rayon)                                           #rayon en une réflexion                         
+         mur_intermediaire_ls = []
+         for wall un murs:                           #2 reflexions
+            if (wall=!= mur):
+                mur_intermediaire_ls.append(wall)
+            image_points(elem[1], mur_intermediaire_ls)
+
+    return list_rayons
+
 
 def relflexion(start_point, end_point, murs):
-    mur_list = []
-    image_points = image_points(start_point, murs)
+    rayons =[]
+    rayon.extend(une_reflexion(start_point,end_point, murs))
 
     
