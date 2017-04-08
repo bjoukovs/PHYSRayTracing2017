178dbfd64726ffd95e7cda1ab49e1e7bbe007407
Maxence Draguet <maxdraguet@hotmail.com>
2017-04-08 11:00:02 +0200

    Rayon direct ajouté

---
+++ direct.py
@@ -0,0 +1,11 @@
+from point import *
+from Rayon import *
+from mur import *
+
+def rayon_direct(start_point,end_point, murs):
+    list_rayon = []
+    nouveau_rayon = Rayon(start_point)                        
+    Rayon.add_points(nouveau_rayon, Point.intersect(start_point, end_point,murs))
+    Rayon.add_point(nouveau_rayon,end_point)
+    list_rayon.append(nouveau_rayon)
+    return list_rayon
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from const import *
 from IO import draw, decode_plan
 from reflexion import rayons_reflexion
+from direct import rayon_direct
 from point import *
 from Rayon import *
 
@@ -10,7 +11,10 @@ COINS = data[8]
 width, height, TXx, TXy, TXorientation, RXx, RXy = data[0],data[1],data[2],data[3],data[4],data[5],data[6]
 
 RAYS_REFLEXION = rayons_reflexion(Point(TXx, TXy), Point(RXx, RXy), MURS)
-
-draw(MURS, RAYS_REFLEXION, width, height, TXx, TXy, RXx, RXy) 
+RAYS_DIRECT =  rayon_direct(Point(TXx, TXy), Point(RXx, RXy), MURS)
+RAYS_AFFICHAGE =[]
+RAYS_AFFICHAGE.extend(RAYS_REFLEXION)
+RAYS_AFFICHAGE.extend(RAYS_DIRECT)
+draw(MURS, RAYS_AFFICHAGE, width, height, TXx, TXy, RXx, RXy) 
 
 
