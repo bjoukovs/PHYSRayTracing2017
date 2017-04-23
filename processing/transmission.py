66d8d7413ceefe3e7f9eaefe879dd92681e2035d
Eliott Ramoisiaux <eliott.ramoisiaux@gmail.com>
2017-04-23 13:45:07 +0200

    get_theta_i et get_tehta_t

---
+++ processing/transmission.py
@@ -0,0 +1,27 @@
+from classes.rayon import *
+from classes.point import *
+from classes.mur import *
+from diffraction import get_direction
+from math import pi as PI
+from math import atan, atan2, sin, sqrt, pow, asin
+
+def get_theta_i (p1,p2):
+    mur = p2.mur 
+    direction = get_direction(p1,p2)
+
+    if mur.is_horizontal():
+        return atan(direction)
+
+    else:
+        return PI - atan(direction)
+
+def get_theta_t (theta_i,eps):
+    n = sqrt(eps)
+    no = sqrt((1/36*PI)*pow(10,-9))
+
+    return asin((no/n)sin(theta_i))
+
+
+
+
+
